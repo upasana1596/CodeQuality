2100c60cf151d22e4681ca9b7913453fd941d76e
upasana <53463882+upasana1596@users.noreply.github.com>
2021-12-29 16:12:36 +0530

    changes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from 'src/navbar/navbar.component';
 import { AddUserComponent } from 'src/registerUser/add-user.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   { path: '', component: NavbarComponent },
-  { path: 'addUser', component:AddUserComponent }
+  { path: 'addUser', component:AddUserComponent },
+  { path: 'user', component:UserComponent }
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NavbarComponent } from 'src/navbar/navbar.component';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AddUserComponent } from 'src/registerUser/add-user.component';
+import { UserComponent } from './user/user.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AddUserComponent,
+    UserComponent,
   ],
   imports: [
     BrowserModule,
@@ -20,9 +24,12 @@ import { AddUserComponent } from 'src/registerUser/add-user.component';
     FormsModule,
     ReactiveFormsModule,
     RouterModule,
-
+    MatTableModule,
+    BrowserAnimationsModule,
+    MatPaginatorModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports:[ MatTableModule,MatPaginatorModule ]
 })
 export class AppModule { }
